d2ead640ba17b377900000d350eb58612b1f524f
JonCabot <jfc0053@gmail.com>
2022-01-30 15:37:24 -0600

    commit tree 9a3630706a9a4951df7d59b460af7dd7e0a1a773

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
-function ProjectList({ projects }) {
-  console.log(projects);
-  return (
+function ProjectList({ projects }) { //this is coming from user.projects, passed in via the App component
+  const renderItem = projects.map((projectItem) => { //map thru each one, assign an id
+    return <ProjectItem 
+              key={projectItem.id} 
+              name={projectItem.name}
+              about={projectItem.about}
+              technologies={projectItem.technologies}
+            />
+    })
+
+  return ( //render each item to the page
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{renderItem}</div>
     </div>
   );
 }
